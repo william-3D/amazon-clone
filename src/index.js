a63753d6ae740692f259f55c67543d74d7b718f0
Sonia William <williamlaw2017@outlook.com>
2023-06-16 12:39:57 +0800

    create root DOM node & add React app render entry point

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css"; //side effect import
+
+const root = ReactDOM.createRoot(document.getElementById("root")); //root DOM node, managed by ReactDOM
+root.render(<App />); //tell React to render React app w/ App as entry point
