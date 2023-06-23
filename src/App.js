e0afe5139256d96f7c701675065b145ea077efd1
Sonia William <williamlaw2017@outlook.com>
2023-06-23 16:24:38 +0800

    define homepage path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HomePage } from "./components";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter> //store current location in browser address bar and allows navigation using built-in history stack
+  );
 };
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+//file created to import multiple components on one line
+export { default as HomePage } from "./HomePage";
