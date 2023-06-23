3f0c5e5842fd30575a3c0d1219d3ed4b4360569a
Sonia William <williamlaw2017@outlook.com>
2023-06-23 16:40:47 +0800

    define central container

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-amazonclone-background">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto"></div>
+    </div>
+  );
 };
 
 export default HomePage;
